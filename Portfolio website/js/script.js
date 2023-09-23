1542374923e3fc734e4b18622ee2b32dbc44f387
atutech <83010998+atutech@users.noreply.github.com>
2023-09-23 01:36:30 +0100

    Add files via upload

---
+++ Portfolio website/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+$('#menu').click(function(){
+   
+    $(this).toggleClass('fa-timtes');
+    $('header').toggleClass ('toggle');
+    $(window).on('scroll load', function(){
+
+});
+
+    $(#menu).removeClass('fa-timtes');
+    $('header').removeClass ('toggle');
+}
+});
